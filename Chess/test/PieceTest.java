f7e66f152f7b1e62021a5a015d3ea9452f8fc34e
ThaGazer <hi5ritter@hotmail.com>
2019-09-13 23:20:39 -0500

    reset test for piece refactoring

--- Chess/test/PieceTest.java
+++ Chess/test/PieceTest.java
@@ -13,20 +13,6 @@ class PieceTest {
 
   @Test
   void test_equals() {
-    assertAll(() -> {
-      assertNotEquals(new Pawn(), new Pawn());
-      assertNotEquals(new Rook(), new Rook());
-      assertNotEquals(new Knight(), new Knight());
-      assertNotEquals(new Bishop(), new Bishop());
-    });
-
-    assertAll(() -> {
-      assertEquals(new Piece(), new Piece());
-      assertEquals(new Queen(), new Queen());
-      assertEquals(new King(), new King());
-      Rook r = new Rook();
-      assertEquals(r, new Rook(r));
-    });
   }
 
   //TODO test moveset somehow
