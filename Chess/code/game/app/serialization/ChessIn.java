f4f497c038c645dce872c1713d5816cf84bd3849
Justin Ritter <hi5ritter@hotmail.com>
2019-11-21 02:24:07 -0600

    created ChessIn.nextToken()

--- Chess/code/game/app/serialization/ChessIn.java
+++ Chess/code/game/app/serialization/ChessIn.java
@@ -1,5 +1,6 @@
 package game.app.serialization;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
@@ -7,20 +8,56 @@ import java.util.Objects;
 public class ChessIn {
 
   private InputStream in;
+  private char deliminator;
 
   public ChessIn(InputStream inputSource) {
-    setInputStream(inputSource);
+    this(inputSource, ' ');
+  }
+
+  public ChessIn(InputStream inputStream, char delim) {
+    setInputStream(inputStream);
+    setDeliminator(delim);
   }
 
   private void setInputStream(InputStream inputSource) {
     in = Objects.requireNonNull(inputSource);
   }
 
+  private void setDeliminator(int delim) {
+    if(delim < 0 || delim > 255) {
+      throw new IllegalArgumentException();
+    }
+    deliminator = (char)delim;
+  }
+
+  private boolean isDeliminator(int bitIn) {
+    return bitIn == deliminator;
+  }
+
+  public boolean isNull() {
+    return in == null;
+  }
+
   public int read() throws ChessSerializationException {
     try {
-      return in.read();
+      int bit;
+      if((bit = in.read()) == -1) {
+        throw new EOFException();
+      } else {
+        return bit;
+      }
     } catch(IOException e) {
       throw new ChessSerializationException(e.getMessage(), e);
     }
+  }
+
+  public String nextToken() throws ChessSerializationException {
+    int bit;
+    StringBuilder ret = new StringBuilder();
+
+    while(!isDeliminator((bit = read()))) {
+      ret.append(bit);
+    }
+    return ret.toString();
   }
 }
